e5639babe62ddf4f62623607a01dfb6d61a05431
jusch98 <jschack98@gmail.com>
2023-10-26 14:20:01 +0200

    ejercicio 3

--- ex3.py
+++ ex3.py
@@ -23,11 +23,13 @@ print("The computer chose: " + computer_choice)
 if user_choice == computer_choice:
   print("It's a tie!")
 elif user_choice == "rock" and computer_choice == "scissors":
-print
-("you win!")
-print
-("you lose")
-
+  print("you win!")
+elif user_choice == "paper" and computer_choice == "rock":
+  print("you win!")
+elif user_choice == "scissors" and computer_choice == "paper":
+  print("you win!")
+else:
+  print("you lose")
 # rock beats scissors
 # paper beats rock
 # scissors beats paper
